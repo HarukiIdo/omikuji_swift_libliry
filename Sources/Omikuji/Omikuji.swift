685587c4d7fc0744df4675a5219638ce00400248
Haruki Ido <rikuharu3705@outlook.jp>
2022-03-19 17:33:17 +0900

    first commit

--- Sources/Omikuji/Omikuji.swift
+++ Sources/Omikuji/Omikuji.swift
@@ -1,6 +1,25 @@
-public struct Omikuji {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public enum Omikuji: String, CaseIterable {
+    case 大吉
+    case 中吉
+    case 小吉
+    case 吉
+    case 末吉
+    case 凶
+    case 大凶
+}
+
+extension Omikuji {
+    
+    public static func draw() -> Self {
+        allCases.randomElement()!
+    }
+}
+
+extension Omikuji: CustomStringConvertible {
+    
+    public var description: String {
+        rawValue
     }
 }
